287141eb7d8c6930530278c1170add9d76e215c7
John Trevor <softwarerecess09@gmail.com>
2023-11-18 23:16:59 +0300

    Initial commit of JobScrapper files

---
+++ jobscrapper.py
@@ -0,0 +1,78 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, jsonify, request # render_template allows us to take the index.html and rendr it when the app is run
+
+app = Flask(__name__) # instantiating the app / creating a flask instance and assigning it to app
+
+def scrape_job_listings(keyword, location):
+    base_url = 'https://www.timesjobs.com/candidate/job-search.html'
+    params = {
+        'searchType': 'personalizedSearch',
+        'from': 'submit',
+        'txtKeywords': keyword,
+        'txtLocation': location,
+    }
+    # print(keyword)
+    # print(location)
+
+    response = requests.get(base_url, params=params)
+    print(response)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        job_elements = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+        job_listings = []
+        for job in job_elements:
+            job_title = job.find('h2').text.strip()
+            company_name = job.find('h3', class_='joblist-comp-name').text.strip()
+            job_location = job.find('ul', class_='top-jd-dtl clearfix').find_all('li')[1].span.text
+            skills = job.find('span', class_='srp-skills').text.strip().replace(' ', '')
+            job_experience = job.find('ul', class_='top-jd-dtl clearfix').find_all('li')[0].text.split('card_travel')[1]
+            publish_date = job.find('span', class_='sim-posted').span.text
+
+            print(job_location)
+
+            # print(f"Job Title: {job_title}")
+            # print(f"Company: {company_name}")
+            # print(f"Location: {job_location}")
+            # print(f"Experience: {job_experience}")
+            # print("=" * 50)
+
+            job_info = {
+                "Job Title": job_title,
+                "Company": company_name,
+                "Location": job_location,
+                "Skills": skills,
+                "Experience": job_experience,
+                "Publish Date": publish_date
+            } # creating a json object for the job information
+
+            job_listings.append(job_info) # appending the job info to the job_listing list
+            # print(job_listings)
+        return job_listings
+    else:
+        return None
+
+@app.route('/jobs', methods=['GET']) # using the route() decorator to tell Flask what URL should trigger our function.    
+def get_jobs():
+    keyword = request.args.get('keyword')
+    location = request.args.get('location')
+    print(keyword)
+    print(location)
+    
+    job_listings = scrape_job_listings(keyword, location)
+    if job_listings:
+        return render_template('index.html', job_listings=job_listings)
+    else:
+        return render_template('index.html', job_listings=None)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # keyword = 'python'  # Update with your desired keyword
+    # location = ''  # Update with your desired location (leave empty for all locations)
+
+    # get_jobs(keyword, location)
+    # scrape_job_listings(keyword, location)
+    # hello_world()
